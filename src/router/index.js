e9272eb1d041c6fde7f8972c800a5ce89a3bc4f7
huangguanglong1998 <2262793475@qq.com>
2021-06-25 01:53:50 +0800

    用户登录-创建页面组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+// 在VueCLI 创建的项目中 @ 表示 src
+// 它是 src 目录的路径别名
+// 好处: 它不受当前文件路径影响
+// 注意: @ 就是 src 路径, 后面别忘了写/
+// 建议: 如果加载的资源路径在当前目录下, 就正常写, 如果需要进行父级路径查找的, 都使用 @
+import Login from '@/views/login/'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
